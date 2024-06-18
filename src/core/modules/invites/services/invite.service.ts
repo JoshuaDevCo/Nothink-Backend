02d42946417386e5c42d55b9480490c7f91b5dca
nevermindhim <scott.brown0629@gmail.com>
2024-06-18 14:10:36 +0300

    fixing invites

--- src/core/modules/invites/services/invite.service.ts
+++ src/core/modules/invites/services/invite.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Invite } from '../entities/invite';
 import { Model } from 'mongoose';
+import { UserService } from '../../auth/modules/user/user.service';
 
 @Injectable()
 export class InviteService {
   constructor(
     @InjectModel(Invite.name) private readonly inviteModel: Model<Invite>,
+    private readonly userService: UserService,
   ) {}
 
   getCollection() {
@@ -26,9 +28,18 @@ export class InviteService {
 
   async getInvited(userId: string): Promise<string[]> {
     const invite = await this.inviteModel.findOne({ from: userId });
-    return invite.accepted_by.map((id) => id.toString());
+    const inviter = await this.inviteModel.findOne({ accepted_by: userId });
+    return [
+      ...invite.accepted_by.map((id) => id.toString()),
+      userId,
+      inviter._id.toString(),
+    ];
   }
   async acceptInvite(userId: string, inviteId: string) {
+    const alreadyAccepted = await this.inviteModel.findOne({
+      accepted_by: userId,
+    });
+    if (alreadyAccepted) throw new Error('Cannot accept twice');
     const invite = await this.inviteModel.findOne({
       _id: inviteId,
       accepted_by: userId,
@@ -46,10 +57,13 @@ export class InviteService {
   }
 
   async claim(userId: string) {
+    const accepter = await this.inviteModel.findOne({ accepted_by: userId });
     const invite = await this.inviteModel.findOne({ from: userId });
     if (!invite) return 0;
-    const diff = invite.accepted_by.length - (invite.claimed || 0);
-    invite.claimed = invite.accepted_by.length;
+    const acceptedReduce = accepter ? 1 : 0;
+    const diff =
+      invite.accepted_by.length + acceptedReduce - (invite.claimed || 0);
+    invite.claimed = invite.accepted_by.length + acceptedReduce;
     await invite.save();
     return diff;
   }
