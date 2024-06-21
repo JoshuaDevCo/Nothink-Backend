f7b8f05ffe36991ce6a02a5f8011834c58f0ddda
nevermindhim <scott.brown0629@gmail.com>
2024-06-21 19:00:44 +0300

    added provider logs

--- src/api/game/services/booster.service.ts
+++ src/api/game/services/booster.service.ts
@@ -36,6 +36,10 @@ export class ApiGameBoosterService {
     this.receiver_address = this.configService.getOrThrow(
       'TON_NETWORK_RECEIVER_ADDRESS',
     );
+    this.logger.log('TON_NETWORK_RECEIVER_ADDRESS: ' + this.receiver_address);
+    this.provider.getBalance(this.receiver_address).then((balance) => {
+      this.logger.debug('TON_NETWORK_RECEIVER_ADDRESS (balance): ' + balance);
+    });
   }
 
   private async getNewlyPaidAmount(
